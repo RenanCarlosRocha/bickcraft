3900f0107fb67b665b15b40ea9f28d8d34730023
Renan Carlos <rc882041@gmail.com>
2022-06-22 18:04:15 -0300

    script de troca de imagem na pagina das bicicletas

--- js/script.js
+++ js/script.js
@@ -37,3 +37,19 @@ function eventosPergunta(pergunta) {
 pergunta.forEach(eventosPergunta);
 
 // Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagem img");
+const galeriaContainer = document.querySelector(".bicicleta-imagem");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width:1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
