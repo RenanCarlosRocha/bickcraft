c16ec75f907516983aba699648f211d0be8542cf
Renan Carlos <rc882041@gmail.com>
2022-06-22 15:29:14 -0300

    scrip de seleção automatica das opções do orçamento

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// Ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
-
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
@@ -8,5 +8,16 @@ function ativarLink(link) {
     link.classList.add("ativo");
   }
 }
-
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
