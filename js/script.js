2e62628b089466e413cadad78607dad82a103928
Renan Carlos <rc882041@gmail.com>
2022-06-22 17:48:45 -0300

    scrip perguntas frequentes de seguros

--- js/script.js
+++ js/script.js
@@ -12,12 +12,28 @@ links.forEach(ativarLink);
 
 // Ativar itens do orçamento
 const parametros = new URLSearchParams(location.search);
-
 function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
   if (elemento) {
     elemento.checked = true;
   }
 }
-
 parametros.forEach(ativarProduto);
+
+// Perguntas frequêntes
+const pergunta = document.querySelectorAll(".perguntas button");
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  const ativo = resposta.classList.contains("ativo");
+
+  pergunta.setAttribute("aria-expanded", ativo);
+  resposta.classList.toggle("ativo");
+}
+function eventosPergunta(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+pergunta.forEach(eventosPergunta);
+
+// Galeria de Bicicletas
